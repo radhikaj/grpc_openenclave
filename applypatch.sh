ed87995cb7791019703d27481a45305eeeb78327
radhikaj <radhikaj@live.com>
2019-06-27 23:29:17 +0000

    apply patch script

--- applypatch.sh
+++ applypatch.sh
@@ -1,3 +1,18 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+##==============================================================================
+
+function trap_handler()
+{
+    echo "ERROR LINE# ${1} : ${2}"
+    exit "${1}"
+}
+
+trap 'trap_handler ${LINENO} $?' ERR
+
 cd openenclave
 git am ../0001-openenclaeve-for-grpc.patch
 cd ../grpc
