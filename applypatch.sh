534b38915b44027e913c5b364b73075aba2f1d93
radhikaj <radhikaj@live.com>
2019-06-27 23:56:23 +0000

    Changed patch

--- applypatch.sh
+++ applypatch.sh
@@ -4,25 +4,24 @@
 # Licensed under the MIT License.
 
 ##==============================================================================
+#function trap_handler()
+#{
+#    echo "ERROR LINE# ${1} : ${2}"
+#    exit "${1}"
+#}
 
-function trap_handler()
-{
-    echo "ERROR LINE# ${1} : ${2}"
-    exit "${1}"
-}
-
-trap 'trap_handler ${LINENO} $?' ERR
+#trap 'trap_handler ${LINENO} $?' ERR
 
 cd openenclave
 git am ../0001-openenclaeve-for-grpc.patch
 cd ../grpc
 git am ../0001-Get-GRPC-to-build.patch
 cd third_party/address_sorting/
-git am ../0001-addresssorting-Get-GRPC-to-build.patch
+git am ../../../0001-addresssorting-Get-GRPC-to-build.patch
 cd ../zlib
-git am ../0001-zlib-CMakeLists.txt.patch
+git am ../../../0001-zlib-CMakeLists.txt.patch
 cd ../protobuf
-git am ../0001-Add-ability-to-build-libprotobuf_oe_enclave.patch
+git am ../../../0001-Add-ability-to-build-libprotobuf_oe_enclave.patch
 cd ../boringssl
-git am ../0001-Change-boringssl-s-cmake-lib-to-crypto2-since-crypto.patch
+git am ../../../0001-Change-boringssl-s-cmake-lib-to-crypto2-since-crypto.patch
 
