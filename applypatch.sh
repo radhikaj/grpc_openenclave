4d0c3099f819f0e7b40d617bb738c488a1acfb02
radhikaj <radhikaj@live.com>
2019-06-28 00:22:24 +0000

    Fix script

--- applypatch.sh
+++ applypatch.sh
@@ -11,17 +11,23 @@
 #}
 
 #trap 'trap_handler ${LINENO} $?' ERR
-
+echo "1"
 cd openenclave
 git am ../0001-openenclaeve-for-grpc.patch
+
+echo "2"
 cd ../grpc
 git am ../0001-Get-GRPC-to-build.patch
-cd third_party/address_sorting/
-git am ../../../0001-addresssorting-Get-GRPC-to-build.patch
-cd ../zlib
+
+echo "3"
+cd third_party/zlib
 git am ../../../0001-zlib-CMakeLists.txt.patch
+
+echo "4"
 cd ../protobuf
 git am ../../../0001-Add-ability-to-build-libprotobuf_oe_enclave.patch
+
+echo "5"
 cd ../boringssl
 git am ../../../0001-Change-boringssl-s-cmake-lib-to-crypto2-since-crypto.patch
 
